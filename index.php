a8091e8266d0e4d89ddae2fbbcac0f8f1a523de8
AlexanderLivanov <help.underlinegroup@gmail.com>
2025-03-27 19:14:47 +0300

    web design

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/swad/css/style.css">
+    <link rel="shortcut icon" href="/swad/static/img/logo.svg" type="image/x-icon">
+
+    <title>Document</title>
+</head>
+
+<body>
+    <?php require_once('swad/static/elements/header.php'); ?>
+    <div class="main-container">
+        <div class="search-field-container">
+            <h1 id="game-search-header">Let's play</h1>
+            <input type="text" id="game-search-input">
+            <input type="submit" id="game-search-submit">
+        </div>
+    </div>
+    <?php require_once('swad/static/elements/footer.php'); ?>
+</body>
+
+</html>
